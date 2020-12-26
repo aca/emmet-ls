914db45a686497403726b0d0d4b4dd2ea7a4bc42
aca <acadx0@gmail.com>
2020-12-26 21:16:30 +0900

    add css support

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # emmet-ls
 
-WIP
+[coc-emmet](https://github.com/neoclide/coc-emmet) replacement for [completion-nvim](https://github.com/nvim-lua/completion-nvim).
+
+![alt](./.image/capture.gif)
 
 Emmet support based on LSP.
+
+#### Install
+```
+npm install -g emmet-ls
+```
+
+#### Configuration 
+
+- [nvim-lspconfig](https://github.com/neovim/nvim-lspconfig)
+  ```lua
+  local nvim_lsp = require'lspconfig'
+  local configs = require'lspconfig/configs'
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+  configs.emmet_ls = {
+    default_config = {
+      cmd = {'emmet-ls', '--stdio'};
+      filetypes = {'html', 'css'};
+      root_dir = function()
+        return vim.loop.cwd()
+      end;
+      settings = {};
+    };
+  }
+
+  nvim_lsp.emmet_ls.setup{
+    on_attach = on_attach;
+  }
+  ```
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,27 @@
 #!/usr/bin/env node
 
 import {
-	createConnection,
-	TextDocuments,
-	Diagnostic,
-	DiagnosticSeverity,
-	ProposedFeatures,
-	InitializeParams,
-	DidChangeConfigurationNotification,
-	CompletionItem,
-	CompletionItemKind,
-	TextDocumentPositionParams,
-	TextDocumentSyncKind,
-	InitializeResult
-} from 'vscode-languageserver';
+  createConnection,
+  TextDocuments,
+  Diagnostic,
+  DiagnosticSeverity,
+  ProposedFeatures,
+  InitializeParams,
+  DidChangeConfigurationNotification,
+  CompletionItem,
+  CompletionItemKind,
+  TextDocumentPositionParams,
+  TextDocumentSyncKind,
+  InitializeResult,
+} from "vscode-languageserver";
 
-import expand from 'emmet';
+import expand, { extract } from "emmet";
 
-import {
-	TextDocument
-} from 'vscode-languageserver-textdocument';
+import { TextDocument } from "vscode-languageserver-textdocument";
 
 let connection = createConnection(ProposedFeatures.all);
 
-// Create a simple text document manager. 
+// Create a simple text document manager.
 let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
 let hasConfigurationCapability: boolean = false;
@@ -31,56 +29,59 @@ let hasWorkspaceFolderCapability: boolean = false;
 let hasDiagnosticRelatedInformationCapability: boolean = false;
 
 connection.onInitialize((params: InitializeParams) => {
-	let capabilities = params.capabilities;
+  let capabilities = params.capabilities;
 
-	// Does the client support the `workspace/configuration` request?
-	// If not, we fall back using global settings.
-	hasConfigurationCapability = !!(
-		capabilities.workspace && !!capabilities.workspace.configuration
-	);
-	hasWorkspaceFolderCapability = !!(
-		capabilities.workspace && !!capabilities.workspace.workspaceFolders
-	);
-	hasDiagnosticRelatedInformationCapability = !!(
-		capabilities.textDocument &&
-		capabilities.textDocument.publishDiagnostics &&
-		capabilities.textDocument.publishDiagnostics.relatedInformation
-	);
+  // Does the client support the `workspace/configuration` request?
+  // If not, we fall back using global settings.
+  hasConfigurationCapability = !!(
+    capabilities.workspace && !!capabilities.workspace.configuration
+  );
+  hasWorkspaceFolderCapability = !!(
+    capabilities.workspace && !!capabilities.workspace.workspaceFolders
+  );
+  hasDiagnosticRelatedInformationCapability = !!(
+    capabilities.textDocument &&
+    capabilities.textDocument.publishDiagnostics &&
+    capabilities.textDocument.publishDiagnostics.relatedInformation
+  );
 
-	const result: InitializeResult = {
-		capabilities: {
-			textDocumentSync: TextDocumentSyncKind.Incremental,
-			// Tell the client that this server supports code completion.
-			completionProvider: {
-				resolveProvider: true
-			}
-		}
-	};
-	if (hasWorkspaceFolderCapability) {
-		result.capabilities.workspace = {
-			workspaceFolders: {
-				supported: true
-			}
-		};
-	}
-	return result;
+  const result: InitializeResult = {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Incremental,
+      // Tell the client that this server supports code completion.
+      completionProvider: {
+        resolveProvider: true,
+      },
+    },
+  };
+  if (hasWorkspaceFolderCapability) {
+    result.capabilities.workspace = {
+      workspaceFolders: {
+        supported: true,
+      },
+    };
+  }
+  return result;
 });
 
 connection.onInitialized(() => {
-	if (hasConfigurationCapability) {
-		// Register for all configuration changes.
-		connection.client.register(DidChangeConfigurationNotification.type, undefined);
-	}
-	if (hasWorkspaceFolderCapability) {
-		connection.workspace.onDidChangeWorkspaceFolders(_event => {
-			connection.console.log('Workspace folder change event received.');
-		});
-	}
+  if (hasConfigurationCapability) {
+    // Register for all configuration changes.
+    connection.client.register(
+      DidChangeConfigurationNotification.type,
+      undefined
+    );
+  }
+  if (hasWorkspaceFolderCapability) {
+    connection.workspace.onDidChangeWorkspaceFolders((_event) => {
+      connection.console.log("Workspace folder change event received.");
+    });
+  }
 });
 
 // The example settings
 interface ExampleSettings {
-	maxNumberOfProblems: number;
+  maxNumberOfProblems: number;
 }
 
 // The global settings, used when the `workspace/configuration` request is not supported by the client.
@@ -93,89 +94,83 @@ let globalSettings: ExampleSettings = defaultSettings;
 let documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
 
 function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
-	if (!hasConfigurationCapability) {
-		return Promise.resolve(globalSettings);
-	}
-	let result = documentSettings.get(resource);
-	if (!result) {
-		result = connection.workspace.getConfiguration({
-			scopeUri: resource,
-			section: 'languageServerExample'
-		});
-		documentSettings.set(resource, result);
-	}
-	return result;
+  if (!hasConfigurationCapability) {
+    return Promise.resolve(globalSettings);
+  }
+  let result = documentSettings.get(resource);
+  if (!result) {
+    result = connection.workspace.getConfiguration({
+      scopeUri: resource,
+      section: "languageServerExample",
+    });
+    documentSettings.set(resource, result);
+  }
+  return result;
 }
 
-documents.onDidClose(e => {
-	documentSettings.delete(e.document.uri);
-  delete contents[e.document.uri];
+documents.onDidClose((e) => {
+  documentSettings.delete(e.document.uri);
 });
 
-let contents: any = {}; 
-
-documents.onDidChangeContent(change => {
-  contents[change.document.uri] = change.document.getText()
-})
-
-
-connection.onDidChangeWatchedFiles(_change => {
-	connection.console.log('We received an file change event');
+connection.onDidChangeWatchedFiles((_change) => {
+  connection.console.log("We received an file change event");
 });
 
 connection.onCompletion(
-	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+  (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
     try {
+      let docs = documents.get(_textDocumentPosition.textDocument.uri);
+      if (!docs) throw "failed to find document";
+      let languageId = docs.languageId;
+      let content = docs.getText();
+      let linenr = _textDocumentPosition.position.line;
+      let line = String(content.split(/\r?\n/g)[linenr]);
+      let character = _textDocumentPosition.position.character;
+      let extractPosition =
+        languageId != "css"
+          ? extract(line, character)
+          : extract(line, character, { type: "stylesheet" });
 
-      let content = contents[_textDocumentPosition.textDocument.uri]
-      let line = String(content.split(/\r?\n/g)[_textDocumentPosition.position.line])
-      connection.console.log(`DEAN: ${line}`)
-
-      let character = _textDocumentPosition.position.character
-      let left = line.slice(0, character).search(/\S+$/)
-      let right = line.slice(character).search(/\S+$/);
-      connection.console.log(`left: ${left}`)
-      connection.console.log(`right: ${right}`)
-
-      let word = "";
-      if (right < 0) {
-        word = line.slice(left);
-      } else {
-        word = line.slice(left, right + character)
+      if (extractPosition?.abbreviation == undefined) {
+        throw "failed to parse line";
       }
-      connection.console.log(`word: ${word}`)
 
-      let expanded = expand(word)
-      
+      let left = extractPosition.start;
+      let right = extractPosition.start;
+      let abbreviation = extractPosition.abbreviation;
+      let expanded =
+        languageId != "css"
+          ? expand(abbreviation)
+          : expand(abbreviation, { type: "stylesheet" });
+
       return [
         {
-          label: word,
+          label: abbreviation,
           detail: "emmet",
           documentation: "documentation",
           textEdit: {
             range: {
               start: {
-                line: _textDocumentPosition.position.line,
+                line: linenr,
                 character: left,
               },
               end: {
-                line: _textDocumentPosition.position.line,
+                line: linenr,
                 character: right,
               },
             },
-            newText: '' + expanded
+            newText: "" + expanded,
           },
           kind: CompletionItemKind.Snippet,
-          data: 1
+          data: 1,
         },
       ];
-      
-    } catch(error) {
-      connection.console.log(`EXX: ${error}`)
+    } catch (error) {
+      connection.console.log(`ERR: ${error}`);
     }
 
-		return [];
-	}
+    return [];
+  }
 );
 
 documents.listen(connection);
